9a07f2e0de9265378aa1f61296ab3f5ec7074331
Roatville <Roatvilless@gmail.com>
2023-09-02 02:55:23 +0530

    Update Name test

--- contact.js
+++ contact.js
@@ -15,43 +15,45 @@ const firebaseConfig = {
   var contactFormDB = firebase.database().ref("contactForm");
   
   document.getElementById("contactForm").addEventListener("submit", submitForm);
-  
-  function submitForm(e) {
+
+function submitForm(e) {
     e.preventDefault();
-  
+
     var name = getElementVal("name");
     var emailid = getElementVal("emailid");
     var msg = getElementVal("msg");
-  
-    if (name === "" || emailid === "" || msg === "") {
-      alert("Please fill in all fields before submitting.");
-      return; // Do not proceed further
-  }
+    var subject = getElementVal("subject"); // Added subject field
 
-    saveMessages(name, emailid, msg);
-  
-    //   enable alert
+    if (name === "" || emailid === "" || msg === "" || subject === "") {
+        alert("Please fill in all fields before submitting.");
+        return; // Do not proceed further
+    }
+
+    saveMessages(name, emailid, msg, subject);
+
+    // Enable alert
     document.querySelector(".alert").style.display = "block";
-  
-    //   remove the alert
+
+    // Remove the alert
     setTimeout(() => {
-      document.querySelector(".alert").style.display = "none";
+        document.querySelector(".alert").style.display = "none";
     }, 3000);
-  
-    //   reset the form
+
+    // Reset the form
     document.getElementById("contactForm").reset();
-  }
-  
-  const saveMessages = (name, emailid, msg) => {
+}
+
+const saveMessages = (name, emailid, msg, subject) => {
     var newContactForm = contactFormDB.push();
-  
+
     newContactForm.set({
-      name: name,
-      emailid: emailid,
-      msg: msg,
+        name: name,
+        emailid: emailid,
+        msg: msg,
+        subject: subject // Added subject field
     });
-  };
-  
-  const getElementVal = (id) => {
+};
+
+const getElementVal = (id) => {
     return document.getElementById(id).value;
-  };
+};
